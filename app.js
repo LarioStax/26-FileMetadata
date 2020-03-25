78a69c140512989939e6a46d048e3f2ef100c931
StaxGit <lario.free@gmail.com>
2020-03-25 12:04:07 +0100

    Add file metadata route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const multer = require("multer");
 
-// let upload = multer(); //don't upload files anywhere, just keep them in memory temporarily
+let upload = multer(); //don't upload files anywhere, just keep them in memory temporarily
 
 const cors = require("cors");
 app.use(cors());
@@ -16,6 +16,18 @@ app.get("/", function (req, res) {
 app.get("/hello", function(req, res) {
 	res.json({greetings: "Hello API!"})
 });
+
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+	if (!req.file) {
+		res.json({"Error": "Please select a file!"});
+	} else {
+		res.json({
+			"name": req.file.originalname,
+			"type": req.file.mimetype,
+			"size": req.file.size
+		})
+	}
+})
 
 let port = process.env.PORT || 3000;
 app.listen(port, function() {
